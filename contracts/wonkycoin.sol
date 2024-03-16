a954a39290dee69003aa45c974a2b5edf7ac1a84
crazybelaruskii <smirnova.yuliya.vlad@gmail.com>
2024-03-16 23:14:58 +0000

    minting

--- contracts/wonkycoin.sol
+++ contracts/wonkycoin.sol
@@ -1,4 +1,7 @@
 pragma solidity ^0.8.19;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
-contract WonkiCoin is ERC20 {}
+contract WonkyCoin is ERC20 {
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
+    }
+}
